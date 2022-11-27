30d0924906b426ad8c3b0221e9ce10b4a40d415a
Denis <golodkov.den@gmail.com>
2022-11-27 21:52:01 +0300

    добавил документацию jsdoc

--- src/Input/Input.js
+++ src/Input/Input.js
@@ -1,6 +1,14 @@
 import s from './Input.module.css';
 import PropTypes from 'prop-types';
 
+/**
+ * Компонент input
+ * @param { string } type Тип поля ввода(input)
+ * @param { string } name Имя поля ввода
+ * @param { string } placeholder Плейсхолдер поля ввода
+ * @param { string } defaultValue Значение поля по умолчанию
+ * @returns поле ввода
+ */
 const Input = ({ type, name, placeholder, defaultValue }) => {
   return (
     <div className={s.root}>
@@ -15,9 +23,21 @@ const Input = ({ type, name, placeholder, defaultValue }) => {
 };
 
 Input.propTypes = {
+  /**
+   * Тип поля ввода
+   */
   type: PropTypes.string,
+  /**
+   * Имя поля ввода
+   */
   name: PropTypes.string,
+  /**
+   * Плейсхолдер поля ввода
+   */
   placeholder: PropTypes.string,
+  /**
+   * Значение по умолчанию поля ввода
+   */
   defaultValue: PropTypes.string
 }
 
